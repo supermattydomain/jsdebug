a7416a64601df08c1775995f8f6aa1530de11857
Matt <matt@supermatty.com>
2011-12-06 10:56:18 +0000

    Initial import: debug output for JavaScript, falling back from console.log to alert().
    git-svn-id: https://svn/svn/jsdebug@2369 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

---
+++ debug.js
@@ -0,0 +1,59 @@
+/**
+ * If true, and if unable to output debug messages unobtrusively,
+ * fall back to showing such messages using alert().
+ * If false, attempt to output debug messages unobtrusively,
+ * or if that fails, silently throw the messages away.
+ */
+var showDebugAlerts = true;
+var maxAlertCount = 5;
+var alertCount;
+
+/**
+ * Displays a string in the 'Web Console' (not 'Error Console') in FireFox 7.0.1 {Win32|Win64|Linux}.
+ * In Internet Explorer, this works only when the 'developer tools' window is open.
+ * @param msg String to be displayed.
+ */
+function debugString(str) {
+	if ("undefined" == typeof(console) || "undefined" == typeof(console.log)) {
+		console = {};
+		console.log = function(str) {
+			if (showDebugAlerts) {
+				if (alertCount++ < maxAlertCount) {
+					alert(str);
+				}
+			} else {
+				// Nowhere unobtrusive to send this string. Drop it on the floor.
+			}
+		};
+	}
+	console.log(str);
+}
+
+/**
+ * Display a list of JavaScript objects of arbitrary type in the browser's debug console.
+ * @param objs Any number of any type of argument - variadic.
+ */
+function debug() {
+	var str = '';
+	for (var i = 0; i < arguments.length; i++) {
+		if ("string" == typeof(arguments[i])) {
+			str += arguments[i];
+		} else {
+			// Loops in IE: debugString(JSON.stringify(JSON.decycle(obj)));
+			str += JSON.stringify(arguments[i]);
+		}
+	}
+	debugString(str);
+}
+
+function debugEvent(funcName, event) {
+	if (event) {
+		debug(funcName + ': event=' + event.type + '; target=' + event.target + '; currentTarget=' + event.currentTarget);
+	} else {
+		debug(funcName + ': event=null');
+	}
+}
+
+$(document).ready(function() {
+	alertCount = 0;
+});
