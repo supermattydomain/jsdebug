343a2c120865de863bf1dd0793fd9b15d13f354d
Matt <matt@supermatty.com>
2011-12-06 12:56:54 +0000

    Remove jQuery dependency, cleanups, improve comments.
    git-svn-id: https://svn/svn/jsdebug@2376 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- debug.js
+++ debug.js
@@ -1,4 +1,9 @@
 /**
+ * Debugging output for JavaScript.
+ * matthew 2011/12/2
+ */
+
+/**
  * If true, and if unable to output debug messages unobtrusively,
  * fall back to showing such messages using alert().
  * If false, attempt to output debug messages unobtrusively,
@@ -6,45 +11,44 @@
  */
 var showDebugAlerts = true;
 var maxAlertCount = 5;
-var alertCount;
+var alertCount = 0;
 
 /**
- * Displays a string in the 'Web Console' (not 'Error Console') in FireFox 7.0.1 {Win32|Win64|Linux}.
- * In Internet Explorer, this works only when the 'developer tools' window is open.
- * @param msg String to be displayed.
+ * In Firefox 7.0.1 {Win32|Win64|Linux}, console.log displays a string in the web console.
+ * In Internet Explorer, it displays in the script tab of the developer tools window,
+ * but it only works when the 'developer tools' window is open at the time that console.log is called.
  */
-function debugString(str) {
-	if ("undefined" == typeof(console) || "undefined" == typeof(console.log)) {
-		console = {};
-		console.log = function(str) {
-			if (showDebugAlerts) {
-				if (alertCount++ < maxAlertCount) {
-					alert(str);
-				}
-			} else {
-				// Nowhere unobtrusive to send this string. Drop it on the floor.
-			}
-		};
-	}
-	console.log(str);
+if ('undefined' == typeof(console)) {
+	console = {};
+}
+if ('function' != typeof(console.log)) {
+	console.log = showDebugAlerts ? function(str) {
+		alert(str);
+	} : function() {
+		// Nowhere unobtrusive to send this string. Drop it on the floor.
+	};
 }
 
 /**
- * Display a list of JavaScript objects of arbitrary type in the browser's debug console.
+ * Display a list of JavaScript objects of arbitrary type.
  * @param objs Any number of any type of argument - variadic.
  */
 function debug() {
+	if (showDebugAlerts && alertCount++ > maxAlertCount) {
+		return;
+	}
 	var str = '';
 	for (var i = 0; i < arguments.length; i++) {
-		if ("string" == typeof(arguments[i])) {
+		if ('string' == typeof(arguments[i])) {
 			str += arguments[i];
-		} else {
-			// FIXME: The following hangs in IE
-			// str += JSON.stringify(JSON.decycle(arguments[i]));
+		} else if ('function' == typeof(JSON.stringify)){
+			// FIXME: Loops in IE: debugString(JSON.stringify(JSON.decycle(obj)));
 			str += JSON.stringify(arguments[i]);
+		} else {
+			str += arguments[i];
 		}
 	}
-	debugString(str);
+	console.log(str);
 }
 
 function debugEvent(funcName, event) {
@@ -54,7 +58,3 @@ function debugEvent(funcName, event) {
 		debug(funcName + ': event=null');
 	}
 }
-
-$(document).ready(function() {
-	alertCount = 0;
-});
