863cfd6ec29478dbacf22f697c08972ba8390554
Matt <matt@supermatty.com>
2011-12-16 09:59:07 +0000

    Only stringify arguments if the output function isn't console.log, which (at least in Mozilla) already understands objects.
    git-svn-id: https://svn/svn/jsdebug@2422 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- debug.js
+++ debug.js
@@ -13,54 +13,63 @@ var maxAlertCount = 5;
 var alertCount = 0;
 
 /**
- * In Firefox 7.0.1 {Win32|Win64|Linux}, console.log displays a string in the
- * web console. In Internet Explorer, it displays in the script tab of the
+ * In Firefox 7.0.1 {Win32|Win64|Linux}, console.log displays any arguments in the
+ * web console, JSON-encoding them as necessary.
+ * In Internet Explorer, it displays in the script tab of the
  * developer tools window, but it only works when the 'developer tools' window
  * is open at the time that console.log is called.
  */
-function debugString(str) {
-	var f;
-	if ('undefined' != typeof(console) && 'undefined' != typeof (console.log)) {
-		f = function(s) {
-			console.log(s);
-		};
+function getDebugFunc(str) {
+	if ('object' == typeof(console) && 'undefined' != typeof (console.log)) {
+		return console.log;
 	} else if (showDebugAlerts && alertCount < maxAlertCount) {
-		f = function(s) {
+		return function(s) {
 			alert(s);
 			alertCount++;
 		};
-	} else {
-		f = function(s) {
-			// Nowhere unobtrusive to send this string. Drop it on the floor.
-		};
 	}
-	f(str);
+	return function(s) {
+		// Nowhere unobtrusive to send this string. Drop it on the floor.
+	};
 }
 
+function argsToString(args) {
+	var str = '';
+	for ( var i = 0; i < args.length; i++) {
+		if ('string' == typeof (args[i])) {
+			str += args[i];
+		} else if ('function' == typeof (JSON.stringify)) {
+			/**
+			 * This either hangs, or takes impossibly long to run.
+			 * Consequently, it is disabled.
+			 */
+			/*
+			if ('function' == typeof (JSON.decycle)) {
+				str += JSON.stringify(JSON.decycle(args[i]));
+			} else {
+				str += JSON.stringify(args[i]);
+			}
+			*/
+			str += JSON.stringify(args[i]);
+		} else {
+			str += args[i];
+		}
+	}
+	return str;
+}
+
+var debugFunc = getDebugFunc();
 /**
  * Display a list of JavaScript objects of arbitrary type.
  * 
  * @param objs
  *            Any number of any type of argument - variadic.
  */
-function debug() {
-	var str = '';
-	for ( var i = 0; i < arguments.length; i++) {
-		if ('string' == typeof (arguments[i])) {
-			str += arguments[i];
-		} else if ('function' == typeof (JSON.stringify)) {
-			// FIXME: Loops in IE:
-			if ('function' == typeof (JSON.decycle)) {
-				str += JSON.stringify(JSON.decycle(arguments[i]));
-			} else {
-				str += JSON.stringify(arguments[i]);
-			}
-		} else {
-			str += arguments[i];
-		}
-	}
-	debugString(str);
-}
+var debug = ("object" == typeof(console) && debugFunc === console.log) ? function() {
+	debugFunc.apply(this, arguments);
+} : function() {
+	debugFunc(argsToString(arguments));
+};
 
 function debugEvent(funcName, event) {
 	if (event) {
